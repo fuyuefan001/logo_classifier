71b43139e353dc65a9dd44eaf3410cebfe968482
fyfhaolihai666 <fu194@purdue.edu>
2019-03-16 15:34:34 -0400

    logo classifier

--- logo_classifier.py
+++ logo_classifier.py
@@ -16,7 +16,6 @@ def cnn_model_fn(features, labels, mode):
     print(input_layer)
     input_layer = tf.cast(input_layer, dtype=tf.float32)
     input_norm = tf.layers.batch_normalization(input_layer)
-    # Convolutional Layer #1
     conv1 = tf.layers.conv2d(
             inputs=input_norm,
             filters=16,
@@ -25,14 +24,8 @@ def cnn_model_fn(features, labels, mode):
             activation=tf.nn.relu,
             name='conv1')
     print(conv1)
-
-    # Pooling Layer #1
-    # conv1_norm = tf.layers.batch_normalization(conv1)
     pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=2)
-    # pool1 = tf.layers.batch_normalization(pool1)
-    # print(pool1)
 
-    # # Convolutional Layer #2 and Pooling Layer #2
     conv2 = tf.layers.conv2d(
         inputs=pool1,
         filters=32,
@@ -40,17 +33,12 @@ def cnn_model_fn(features, labels, mode):
         padding="valid",
         activation=tf.nn.relu)
     print(conv2)
-    # conv2 = tf.layers.batch_normalization(conv2)
     pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-    # pool2 = tf.layers.batch_normalization(pool2)
     print(pool2)
 
     pool2_flat = tf.reshape(pool2, [-1, 9*9*32])
-    # dconv1=tf.layers.conv2d_transpose()
     # Dense Layer
     dense1 = tf.layers.dense(inputs=pool2_flat, units=128, activation=tf.nn.relu)  # 36/2
-    # dropout1 = tf.layers.dropout(
-    #     inputs=dense1, rate=0, training=mode == tf.estimator.ModeKeys.TRAIN)
     dense2 = tf.layers.dense(inputs=dense1, units=24, activation=tf.nn.relu)
     dense3 = tf.layers.dense(inputs=dense2, units=2, activation=tf.nn.relu)
     dropout = tf.layers.dropout(
@@ -99,13 +87,6 @@ def main(unused_argv):
     # Load training and eval data
     datax = np.load('x.npy')
     datay = np.load('y.npy')
-    # datax = datax.reshape([-1, 40, 40, 6])
-    # my_feature_columns = [tf.feature_column.numeric_column(key='x', shape=[14400])]
-    # train_data = datax[0:340]  # Returns np.array
-    # train_labels = datay[0:340]
-    #
-    # eval_data = datax[300:340]
-    # eval_labels = datay[300:340]
     train_data=[]
     train_labels=[]
     eval_data=[]
@@ -152,9 +133,6 @@ def main(unused_argv):
     print(eval_results)
 
 
-tf.logging.set_verbosity(tf.logging.INFO)
-
-# Our application logic will be added here
 
 if __name__ == "__main__":
     tf.app.run()
